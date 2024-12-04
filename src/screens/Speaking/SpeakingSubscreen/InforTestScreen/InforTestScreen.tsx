cd56e1de939be6f79a664e35c0c8eff8ab85266b
chankiet1804 <22520717@gm.uit.edu.vn>
2024-12-04 19:48:23 +0700

    add SpeakingSubscreen and update SpeakingScreen

---
+++ src/screens/Speaking/SpeakingSubscreen/InforTestScreen/InforTestScreen.tsx
@@ -0,0 +1,12 @@
+
+import { Text } from "react-native";
+import { SafeAreaBox } from "../../../../components";
+import { SpeakingScreenProps } from "../../../types";
+
+export function InforTestScreen() {
+  return (
+    <SafeAreaBox>
+      <Text>InforTestScreen</Text>
+    </SafeAreaBox>
+  );
+}
